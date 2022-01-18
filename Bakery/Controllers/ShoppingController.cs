463bb4d3ca45d65dbfb7c9951b020c3ce921e085
wiktomi <74141843+wiktomi@users.noreply.github.com>
2022-01-19 00:39:49 +0100

    Adding ShoppingController and action do adding products to cart in db

---
+++ Bakery/Controllers/ShoppingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
+using AutoMapper;
+
+namespace Bakery.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        BakeryAzureEntities db = new BakeryAzureEntities();
+
+         
+
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        [HttpGet]
+        public ActionResult AddToCart(int id)
+        {
+            var prod = db.Products.SingleOrDefault(p => p.ProductID == id);
+
+            var shoppingcart = new ShoppingCart()
+            {
+                ProductName = prod.ProductName,
+                Price = prod.ProductPrice,
+                StartDate = DateTime.Now
+            };
+            db.ShoppingCarts.Add(shoppingcart);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
